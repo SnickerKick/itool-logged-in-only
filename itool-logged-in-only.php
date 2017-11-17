09f1d007eb5f6f3e45688472b7826f43b121b375
Ron Zandbergen <ron@insight-tools.com>
2017-11-17 17:05:50 -0600

    Version 1.0 - Initial base functionality works

--- itool-logged-in-only.php
+++ itool-logged-in-only.php
@@ -9,9 +9,56 @@ Author URI: https://www.insight-tools.com
 License: GPLv2
 */
 
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 function logged_in_only() {
-	if ( ! is_user_logged_in() ) {
-		auth_redirect();
+	if ( ! is_user_logged_in() && ! is_page( esc_attr( get_option('not-logged-page') ))) {
+		wp_redirect( get_option( 'not-logged-page' ));
+                exit();
 	}
 }
 add_action( 'template_redirect', 'logged_in_only' );
+
+add_action( 'admin_menu', 'my_plugin_menu' );
+
+/** Step 1. */
+function my_plugin_menu() {
+    // Create the options page
+    add_options_page( 'Logged In Only Options', 'Logged In Only', 'manage_options', 'itool-logged-in-only-options', 'my_plugin_options_page' );
+        
+    // Call register settings function
+    add_action( 'admin_init', 'register_plugin_settings');
+}
+
+function register_plugin_settings() {
+    // register the plugin's settings
+    register_setting('itool-settings-group', 'not-logged-page');
+}
+
+/** Step 3. */
+function my_plugin_options_page() {
+    if ( !current_user_can( 'manage_options' ) )  {
+    	wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    }
+?>
+<div class="wrap">
+<h1>Logged In Only Options</h1>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'itool-settings-group' ); ?>
+    <?php do_settings_sections( 'itool-settings-group' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Page to display if not logged in</th>
+        <td><input type="text" name="not-logged-page" value="<?php echo esc_attr( get_option('not-logged-page') ); ?>" /></td>
+        </tr>
+    </table>
+    
+    <?php submit_button(); ?>
+
+</form>
+</div>
+<?php } ?>
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Logged-in-only ===
+Contributors: janreim, drivingralle
+Tags: authentication, login, password, user, redirect, simple
+Requires at least: 3.0.1
+Tested up to: 4.9
+Stable tag: 1.0.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A Plugin to lock down the whole site to prevent public access.
+
+== Description ==
+
+Only logged-in users can view the site if this plugin is activated.
+
+== Installation ==
+
+1. Upload `logged-in-only.php` to the `wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Your site is locked down to the public.
+
+== Changelog ==
+
+= 1.0.3 - 2016-11-17 =
+* New main plugin maintainer
+* Tested for WordPress 4.9
+
+= 1.0.2 =
+* Codestyling by drivingralle
+
+= 1.0.1 =
+* initial commit
